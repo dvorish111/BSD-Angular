dbb9a4cc671c401d12d814965f18ee86c9d570b9
chanaBarn <149392221+chanaBarn@users.noreply.github.com>
2023-11-27 12:04:39 +0200

    add navbar routing

--- Campain/src/app/app-routing.module.ts
+++ Campain/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LogInComponent } from './log-in/log-in.component';
 import { HomeComponent } from './home/home.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { AdminInterfaceComponent } from './admin-interface/admin-interface.component';
+import { ContactComponent } from './contact/contact.component';
+import { FamiliesComponent } from './families/families.component';
 
 const routes: Routes = [
  { path: 'logIn',  component: LogInComponent },
  {  path: 'signUp',  component: SignUpComponent },
  {  path: 'home',  component: HomeComponent },
- {  path: 'contact',  component: HomeComponent },
- {  path: 'families',  component: HomeComponent },
+ {  path: 'contact',  component: ContactComponent },
+ {  path: 'families',  component: FamiliesComponent },
  {  path: 'admin-interface',  component: AdminInterfaceComponent },
 
 ];
