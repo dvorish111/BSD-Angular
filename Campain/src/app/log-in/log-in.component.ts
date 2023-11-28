84b2f1d2c225d91ba2bdc85e3c4e97b7417b5738
dvorish111 <dvorish111@gmail.com>
2023-11-28 13:46:23 +0200

    END LOGIN

--- Campain/src/app/log-in/log-in.component.ts
+++ Campain/src/app/log-in/log-in.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { PermissionService } from '../Services/permission.service';
 import { Parser } from '@angular/compiler';
 import { LogIn } from '../Classes/LogIn';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
@@ -12,20 +13,44 @@ import { LogIn } from '../Classes/LogIn';
 export class LogInComponent {
  
 
-  constructor(public myRouter: Router, public permissionSer: PermissionService) {
-  }
-  user: LogIn = {email: '', password: '' };
-  login() {
+  loginForm: FormGroup;
+  model: any;
+  User: LogIn = {
+    password: '',
+    email: ''
+  };
+  constructor(private myRouter: Router, private permissionSer: PermissionService) {
+    this.loginForm = new FormGroup({
+      Email: new FormControl("", [Validators.required, Validators.minLength(5), Validators.email]),
+      Password: new FormControl("", [Validators.required, Validators.minLength(6), Validators.pattern(/^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$/)])
+    });
 
-    this.permissionSer.getByPassword_Email(this.user.password,this.user.email).subscribe({
-      next: (res) => { alert(res)
+  }
+
+  ngOnInit() { }
+
+
+  get formControls() {
+    return this.loginForm.controls;
+  }
+  submitLoginForm(): void {
+    if (this.loginForm.valid) {
+      this.User.password = this.loginForm.value.Password;
+      this.User.email = this.loginForm.value.Email;
+    }
+    this.permissionSer.getByPassword_Email(this.User.password,this.User.email).subscribe({
+      next: (res) => {
+        alert("successful")
         this.myRouter.navigate(['/admin-interface'])
       },
       error: (err) => {
-        console.log(' error: ' + err);
+        alert("no user")
       }
     })
   }
 
+  
+  }
+
  
-}
+
--- Campain/src/app/sign-up/sign-up.component.ts
+++ Campain/src/app/sign-up/sign-up.component.ts
@@ -10,29 +10,43 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent implements OnInit{
-  loginForm:FormGroup;
+export class SignUpComponent implements OnInit {
+  loginForm: FormGroup;
   model: any;
-  NewUser: SignUp | undefined ;
-  constructor(private myRouter: Router, private permissionSer: PermissionService,private formBuilder: FormBuilder) {
-      this.loginForm = new FormGroup({
-      ManagerName: new FormControl() ,
-      Email: new FormControl("", [Validators.required,Validators.minLength(5), Validators.email]),
-      Password:new FormControl( "", [Validators.required, Validators.minLength(6), Validators.pattern(/^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$/)])
-      
-      });
-  } 
-  ngOnInit() {  } 
-  
-  
+  NewUser: SignUp = {
+    password: '',
+    managerName: '',
+    email: ''
+  };
+  constructor(private myRouter: Router, private permissionSer: PermissionService, private formBuilder: FormBuilder) {
+    this.loginForm = new FormGroup({
+      ManagerName: new FormControl(),
+      Email: new FormControl("", [Validators.required, Validators.minLength(5), Validators.email]),
+      Password: new FormControl("", [Validators.required, Validators.minLength(6), Validators.pattern(/^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$/)])
+    });
+
+  }
+
+  ngOnInit() { }
+
+
   get formControls() {
     return this.loginForm.controls;
   }
-  submitLoginForm() {
-    if (this.loginForm.invalid) {
-      return;}
-      this.NewUser = this.loginForm.value;
-      }
-     
-  
+  submitLoginForm(): void {
+    if (this.loginForm.valid) {
+      this.NewUser.password = this.loginForm.value.Password;
+      this.NewUser.email = this.loginForm.value.Email;
+      this.NewUser.managerName = this.loginForm.value.ManagerName;
     }
+    this.permissionSer.createPermission(this.NewUser).subscribe({
+      next: (res) => {
+        alert("successful")
+        this.myRouter.navigate(['/admin-interface'])
+      },
+      error: (err) => {
+        console.log(' error: ' + err);
+      }
+    })
+  }
+}
