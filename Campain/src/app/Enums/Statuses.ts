36bf3cbe2f653333c1da0793377faf37b9473787
dvorish111 <dvorish111@gmail.com>
2023-11-30 10:28:54 +0200

    add enum statuses

---
+++ Campain/src/app/Enums/Statuses.ts
@@ -0,0 +1,5 @@
+export enum Statuses {
+  'Married' = 0,
+  'Divorced' = 1,
+  'Widow' = 2
+}
--- Campain/src/app/families/families.component.ts
+++ Campain/src/app/families/families.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { DonateService } from '../Services/donate.service';
 import { Donate } from '../Classes/Donate';
+import { Statuses } from '../Enums/Statuses';
+
 
 @Component({
   selector: 'app-families',
@@ -9,6 +11,8 @@ import { Donate } from '../Classes/Donate';
   styleUrls: ['./families.component.css']
 })
 export class FamiliesComponent {
+ status:Statuses | undefined;
+
   constructor(public myRouter: Router, private donateService: DonateService) {
 
   }
@@ -26,5 +30,25 @@ export class FamiliesComponent {
   });
 }
 
+  
 
-}
+    needed(needed: number) {
+      this.donateService.getAllByNeeded(needed).subscribe
+  ({
+    next: (donate:Donate[]) => {
+             
+      console.log(donate);
+
+    },
+    error: (err) => {
+      console.error(err);
+    }
+  });
+      }
+
+      funded(funded: boolean) {
+       
+            }
+        }
+
+
