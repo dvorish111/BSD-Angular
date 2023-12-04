37bfca2f6785ae352514558441c231a77767c2e1
dvorish111 <dvorish111@gmail.com>
2023-12-04 14:21:03 +0200

    end filters

--- Campain/src/app/families/families.component.ts
+++ Campain/src/app/families/families.component.ts
@@ -5,6 +5,8 @@ import { Donate } from '../Classes/Donate';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Statuses } from '../Enums/Statuses';
+import { Neighborhood } from '../Classes/Neighborhood';
+import { NeighborhoodService } from '../Services/neighborhood.service';
 
 
 @Component({
@@ -19,11 +21,14 @@ import { Statuses } from '../Enums/Statuses';
 
 export class FamiliesComponent implements OnInit {
 
+  neighborhoods!:Neighborhood[];
   donates!: Donate[];
   raised!: number;
+  donatesN!: Donate[];
+  tempdonates =this.donates;
 
   Statuses= Statuses ;
-  constructor(private donateService: DonateService, public myRouter: Router) {
+  constructor(private donateService: DonateService, public myRouter: Router,private neighborhoodService:NeighborhoodService) {
 
   }
 
@@ -34,6 +39,8 @@ export class FamiliesComponent implements OnInit {
         next: (donates: Donate[]) => {
           this.donates = donates;
           console.log(this.donates);
+          this.getAllNeighborhoods();
+         this.tempdonates =this.donates;
         },
         error: (err) => {
           console.error(err);
@@ -75,15 +82,39 @@ export class FamiliesComponent implements OnInit {
 
   funded(funded: boolean) {
     
-   
   }
 
   status(status:number){
     this.donateService.getAllStatus(status).subscribe
     ({
-      next: (donates: Donate[]) => {
+        next: (donates: Donate[]) => {
         this.donates = donates;
         console.log(donates);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+    
+  }
+
+  neighborhood(neighborhoodId:number)
+  {this.donates=this.tempdonates;
+    this.donatesN = [];
+    this.donates.forEach(element => {
+    if(element.idNeighborhood==neighborhoodId)
+    {this.donatesN.push(element)}    
+   });
+    this.tempdonates =this.donates;
+    this.donates=this.donatesN;
+  }
+
+  getAllNeighborhoods(){
+    this.neighborhoodService.getAllNeighborhoods().subscribe
+    ({
+      next: (neighborhood:Neighborhood[]) => {
+        this.neighborhoods = neighborhood;
+        console.log(this.neighborhoods);
 
       },
       error: (err) => {
