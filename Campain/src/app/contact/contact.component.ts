ddb437f85967ab86593be85a10124654c9164d80
dvorish111 <dvorish111@gmail.com>
2023-12-04 15:07:36 +0200

    add family component in home

--- Campain/src/app/contact/contact.component.ts
+++ Campain/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = new FormGroup({
+      firstName:new FormControl ('', Validators.required),
+      lastName:new FormControl ("", Validators.required),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      message:new FormControl ('', Validators.required)
+    });
+
+  
+  }
+
+  onSubmit() {
+    // Handle the form submission here
+    if (this.contactForm.valid) {
+      // Perform contact processing logic
+      console.log('Contact form submitted successfully');
+      console.log(this.contactForm.value);
+    } else {
+      // Handle invalid form submission
+      console.log('Invalid form submission');
+    }
+  }
 }
+
