c6c2dffbced5f35ddfe55e38d8faec088d6199a4
chanaBarn <149392221+chanaBarn@users.noreply.github.com>
2024-01-10 11:47:21 +0200

    paiment pay

--- Campain/src/app/payment/payment.component.ts
+++ Campain/src/app/payment/payment.component.ts
@@ -1,6 +1,9 @@
 import { Component ,AfterViewInit, OnInit, Renderer2 } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NeighborhoodService } from '../Services/neighborhood.service';
+import { Neighborhood } from '../Classes/Neighborhood';
 import '../../assets/check/script.js';
 @Component({
   selector: 'app-payment',
@@ -8,14 +11,40 @@ import '../../assets/check/script.js';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent  implements OnInit {
-  constructor(private renderer: Renderer2) { }
-
+  constructor(private renderer: Renderer2,private route:Router,private activatedRoute:ActivatedRoute,private neighborhoodService:NeighborhoodService) { }
+  selectedPaymentType:string='Ragil';
+  selectedNeighborhood!:Neighborhood;
+  amount!:string|null;
+  neighborhoods!:Neighborhood[];
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(params => {
+      var amount1=params.get('amount');
+      this.amount=amount1;
+     
+      });
+
+      this.getAllNeighborhoods();
+
     this.loadScript();
    
       window.addEventListener('message', this.receiveMessage.bind(this), false);
     }
-    
+    getAllNeighborhoods(){
+      this.neighborhoodService.getAllNeighborhoods().subscribe
+      ({
+        next: (neighborhood:Neighborhood[]) => {
+          this.neighborhoods = neighborhood;
+          console.log(this.neighborhoods);
+  
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      });
+    }  
+
+   
+
     public receiveMessage(event: MessageEvent): void {
       if (event.origin !== '../../assets/check/script.js') {
         return; // Ignore messages from other origins for security
@@ -35,6 +64,8 @@ export class PaymentComponent  implements OnInit {
     
    
   }
-
+  PaymentTypeClick(selectedPaymentType:string){
+    this.selectedPaymentType=selectedPaymentType;
+  }
   
 }
